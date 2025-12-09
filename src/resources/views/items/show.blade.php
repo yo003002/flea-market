8da0a81a23276f0a292c5a8b121af53e178ec39e
choco <chocolate.so.yummy@icloud.com>
2025-12-09 16:22:51 +0900

    商品詳細画面作成

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -2,8 +2,56 @@
  @extends('layouts.app')
 
  @section('css')
+ <link rel="stylesheet" href="{{ asset('css/items/show.css') }}">
  @endsection
 
  @section('content')
- <h2>商品詳細画面</h2>
+ <div class="show-content">
+    <div class="show-content__goods">
+        <div class="show-content__goods-img">
+            <img src="" alt="">todoここに商品画像を入れる
+        </div>
+        <div class="show-content__goods-detail">
+            <div class="goods-title">
+                <h3>todo商品名をここに入れる</h3>
+            </div>
+            <div class="goods-brand">
+                <p>todoブランド名を入れる</p>
+            </div>
+            <div class="goods-price">
+                <p>todo金額を入れる</p>
+            </div>
+            <div class="goods-heart-comment">
+                <p>todoいいねのハートとコメントのimgを入れる</p>
+            </div>
+            <div class="goods-buy__btn">
+                <a href="/">購入手続きへ</a>
+            </div>
+            <div class="goods-explain">
+                <h3>商品説明</h3>
+            </div>
+            <div class="goods-explain__content">
+                <p>todo商品の詳細を入れる</p>
+            </div>
+            <div class="goods-information">
+                <h3>商品の情報</h3>
+            </div>
+            <div class="goods-infomation__status">
+                <p>todoここにカテゴリーと商品の状態を入れる</p>
+            </div>
+            <div class="user-infomation">
+                <p>todoここにコメント数・ユーザーの名前と写真とコメント</p>
+            </div>
+            <div class="goods-comment-title">
+                <p>商品へのコメント</p>
+            </div>
+            <div class="gooods-comment__textarea">
+                <textarea name="" id=""></textarea>
+            </div>
+            <div class="goods-comment__btn">
+                <a href="">コメントを送信する</a>
+            </div>
+        </div>
+    </div>
+ </div>
  @endsection
