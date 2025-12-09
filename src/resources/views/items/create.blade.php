d2275e59a203df23c231a064e71b0c7365ebd556
choco <chocolate.so.yummy@icloud.com>
2025-12-10 04:28:13 +0900

    商品出品画面作成

--- src/resources/views/items/create.blade.php
+++ src/resources/views/items/create.blade.php
@@ -2,8 +2,69 @@
  @extends('layouts.app')
 
  @section('css')
+ <link rel="stylesheet" href="{{ asset('css/items/create.css') }}">
  @endsection
 
  @section('content')
- <h2>商品出品画面</h2>
+ <div class="sell-content">
+    <div class="sell-item__title">
+        <h2>商品の出品</h2>
+    </div>
+    <form action="" class="sell-item__form">
+        <div class="sell-item__img-title">
+            <p>商品画像</p>
+        </div> 
+        <div class="sell-item__img">
+            <p>todoここに画像を選択するのボタンを追加</p>
+        </div>
+        <div class="sell-item__detail">
+            <h3>商品の詳細</h3>
+        </div>
+        <div class="sell-item__category-title">
+            <p>カテゴリー</p>
+        </div>
+        <div class="sell-item__category">
+            <p>todoここにカテゴリー一覧を入れて選択できるようにする</p>
+        </div>
+        <div class="sell-item__status-title">
+            <p>商品の状態</p>
+        </div>
+        <div class="sell-item__status">
+            <select name="" id="">
+                <option disabled selected>選択してください</option>
+            </select>
+        </div>
+        <div class="sell-item__name-explain-title">
+            <h3>商品名と説明</h3>
+        </div>
+        <div class="sell-item__name-title">
+            <p>商品名</p>
+        </div>
+        <div class="sell-item__name-input">
+            <input type="text">
+        </div>
+        <div class="sell-item__brand-name-title">
+            <p>ブランド名</p>
+        </div>
+        <div class="sell-item__brand-name-input">
+            <input type="text">
+        </div>
+        <div class="sell-item__explain-title">
+            <p>商品の説明</p>
+        </div>
+        <div class="sell-item__explain-textarea">
+            <textarea name="" id=""></textarea>
+        </div>
+        <div class="sell-item__price-title">
+            <p>販売価格</p>
+        </div>
+        <div class="sell-item__price-input">
+            <input type="text">
+        </div>
+        <div class="sell-item__btn">
+            <button class="sell-item__btn-submit" type="submit">出品する</button>
+        </div>
+    </form>
+ </div>
+ 
  @endsection
--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -7,51 +7,54 @@
 
  @section('content')
  <div class="show-content">
-    <div class="show-content__goods">
-        <div class="show-content__goods-img">
-            <img src="" alt="">todoここに商品画像を入れる
+    <div class="show-content__item">
+        <div class="show-content__item-img">
+            <img src="" alt="">
+            <p>todoここに商品画像を入れる</p>
         </div>
-        <div class="show-content__goods-detail">
-            <div class="goods-title">
-                <h3>todo商品名をここに入れる</h3>
+        <form class="show-content__form" action="">
+            <div class="item-detail">
+                <div class="item-detail__title">
+                    <h3>todo商品名をここに入れる</h3>
+                </div>
+                <div class="item-detail__brand">
+                    <p>todoブランド名を入れる</p>
+                </div>
+                <div class="item-detail__price">
+                    <p>todo金額を入れる</p>
+                </div>
+                <div class="item-detail__heart-comment">
+                    <p>todoいいねのハートとコメントのimgを入れる</p>
+                </div>
+                <div class="item-detail__buy-btn">
+                    <a href="/">購入手続きへ</a>
+                </div>
+                <div class="item-detail__explain">
+                    <h3>商品説明</h3>
+                </div>
+                <div class="item-detail__explain-content">
+                    <p>todo商品の詳細を入れる</p>
+                </div>
+                <div class="item-detail__information">
+                    <h3>商品の情報</h3>
+                </div>
+                <div class="item-detail__information-status">
+                    <p>todoここにカテゴリーと商品の状態を入れる</p>
+                </div>
+                <div class="item-detail__user-information">
+                    <p>todoここにコメント数・ユーザーの名前と写真とコメント</p>
+                </div>
+                <div class="item-detail__comment-title">
+                    <p>商品へのコメント</p>
+                </div>
+                <div class="item-detail__comment-textarea">
+                    <textarea name="" id=""></textarea>
+                </div>
+                <div class="item-detail__comment-btn">
+                    <button class="item-detail__comment-btn-submit" type="submit">コメントを送信する</button>
+                </div>
             </div>
-            <div class="goods-brand">
-                <p>todoブランド名を入れる</p>
-            </div>
-            <div class="goods-price">
-                <p>todo金額を入れる</p>
-            </div>
-            <div class="goods-heart-comment">
-                <p>todoいいねのハートとコメントのimgを入れる</p>
-            </div>
-            <div class="goods-buy__btn">
-                <a href="/">購入手続きへ</a>
-            </div>
-            <div class="goods-explain">
-                <h3>商品説明</h3>
-            </div>
-            <div class="goods-explain__content">
-                <p>todo商品の詳細を入れる</p>
-            </div>
-            <div class="goods-information">
-                <h3>商品の情報</h3>
-            </div>
-            <div class="goods-infomation__status">
-                <p>todoここにカテゴリーと商品の状態を入れる</p>
-            </div>
-            <div class="user-infomation">
-                <p>todoここにコメント数・ユーザーの名前と写真とコメント</p>
-            </div>
-            <div class="goods-comment-title">
-                <p>商品へのコメント</p>
-            </div>
-            <div class="gooods-comment__textarea">
-                <textarea name="" id=""></textarea>
-            </div>
-            <div class="goods-comment__btn">
-                <a href="">コメントを送信する</a>
-            </div>
-        </div>
+        </form>
     </div>
  </div>
  @endsection
